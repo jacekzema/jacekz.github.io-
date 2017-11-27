e69df00b233eccbd240e3c3be6fd2b2183cf6f23
Jacek Zema <zemajacek@gmai.com>
2017-11-27 23:14:29 +0100

    pierwszy commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#jacekz.github.io
---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () 
+{
+    $(".cross").hide();
+    $(".menu").hide();
+    
+    $( ".hamburger" ).click(function() 
+	{
+		$( ".menu" ).slideToggle( "slow", function() 
+        {   
+		  $( ".hamburger" ).fadeOut(50);
+		  $( ".cross" ).show(120);
+		});
+    });
+    
+    $( ".cross" ).click(function() 
+	{
+		$( ".menu" ).slideToggle( "slow", function() 
+        {
+		  $( ".cross" ).fadeOut(50);
+		  $( ".hamburger" ).show(120);
+		});
+    });
+
+});
